a444a92a75b853448faf7024d213eedfc3c5d55c
bignick26 <nickrestiv@gmail.com>
2019-10-22 22:03:29 -0400

    Build for revo-mini

---
+++ build/revo-mini/ap_config.h
@@ -0,0 +1,21 @@
+/* WARNING! All changes made to this file will be lost! */
+
+#ifndef W_REVO_MINI_AP_CONFIG_H_WAF
+#define W_REVO_MINI_AP_CONFIG_H_WAF
+
+#define WAF_BUILD 1
+#define __STDC_FORMAT_MACROS 1
+/* #undef HAVE_CMATH_ISFINITE */
+/* #undef HAVE_CMATH_ISINF */
+/* #undef HAVE_CMATH_ISNAN */
+/* #undef NEED_CMATH_ISFINITE_STD_NAMESPACE */
+/* #undef NEED_CMATH_ISINF_STD_NAMESPACE */
+/* #undef NEED_CMATH_ISNAN_STD_NAMESPACE */
+/* #undef HAVE_ENDIAN_H */
+/* #undef HAVE_BYTESWAP_H */
+/* #undef HAVE_MEMRCHR */
+#define PYTHONDIR "/usr/lib/python2.7/site-packages"
+#define PYTHONARCHDIR "/usr/lib/python2.7/site-packages"
+#define _GNU_SOURCE 1
+
+#endif /* W_REVO_MINI_AP_CONFIG_H_WAF */
